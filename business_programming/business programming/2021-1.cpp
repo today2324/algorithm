c64614ab6b9f408a6394b4dec37156ba3469e774
CENSORED <55275098+today2324@users.noreply.github.com>
2023-05-18 10:35:39 +0900

    Update 2021-1.cpp

--- business_programming/business programming/2021-1.cpp
+++ business_programming/business programming/2021-1.cpp
@@ -3,82 +3,44 @@ using namespace std;
 
 int main()
 {
-	int sod, sodg, seg;
-	long long int gwa;
-	long long int sose = 0;
-	cin >> sod >> sodg >> seg;
-	gwa = sod - sodg;
-	if (gwa > 0)
-	{
-		if (gwa <= 46000000)
-		{
-			sose += gwa * 0.06;
-		}
-		else
-		{
-			sose += 12000000 * 0.06;
-		}
-	}
-	if (12000000 < gwa)
-	{
-		if (gwa <= 46000000)
-		{
-			sose += gwa * 0.15;
-		}
-		else
-		{
-			sose += (46000000- 12000000) * 0.15;
-			cout << (46000000 - 12000000) * 0.15<<endl;
-		}
-	}
-	if (46000000 < gwa)
-	{ 
-		if (gwa <= 88000000)
-		{
-			sose += gwa * 0.24 ;
-		}
-		else
-		{
-			sose +=( 88000000- 46000000) * 0.24 ;
-		}
-	}
-	if (88000000 < gwa)
-	{
-		if (gwa <= 150000000)
-		{
-			sose += gwa * 0.35;
-		}
-		else
-		{
-			sose += (150000000- 88000000) * 0.35;
-		}
-	}
-	if (150000000 < gwa)
-	{
-		if (gwa <= 300000000)
-		{
-			sose += gwa * 0.38;
-		}
-		else
-		{
-			sose += (300000000- 150000000 ) * 0.38;
-		}
-	}
-	if (300000000 < gwa)
-	{
-		if (gwa <= 500000000)
-		{
-			sose += gwa * 0.4;
-		}
-		else
-		{
-			sose += (500000000-300000000) * 0.4;
-		}
-	}
-	else
-	{
-		sose += gwa * 0.42;
-	}
+	int sod, sodgo, segam;
+	int total_sod = 0;
+	int gwa;
+	cin >> sod >> sodgo >> segam;
 
-	cout << sose - seg;
+	gwa = sod - sodgo;
+
+	if (0 < gwa)
+	{
+		if (gwa <= 12000000)
+			total_sod += gwa * 0.06;
+		else
+			total_sod += 12000000 * 0.06;
+
+		if (12000000 < gwa && gwa <= 46000000)
+			total_sod += (gwa - 12000000) * 0.15;
+		else if(12000000 < gwa)
+			total_sod += (46000000 - 12000000) * 0.15;
+
+		if (46000000 < gwa && gwa <= 88000000)
+			total_sod += (gwa - 46000000) * 0.24;
+		else if(46000000 < gwa)
+			total_sod += (88000000 - 46000000) * 0.24;
+
+		if (88000000<gwa && gwa <= 150000000)
+			total_sod += (gwa - 88000000) * 0.35;
+		else if(88000000 < gwa)
+			total_sod += (150000000 - 88000000) * 0.35;
+
+		if (150000000<gwa && gwa <= 300000000)
+			total_sod += (gwa - 150000000) * 0.38;
+		else if(150000000 < gwa)
+			total_sod += (300000000 - 150000000) * 0.38;
+
+		if (300000000<gwa && gwa <= 500000000)
+			total_sod += (gwa - 300000000) * 0.42;
+		else if(300000000 < gwa)
+			total_sod += (500000000 - 300000000) * 0.42;
+	}
+	cout << total_sod - segam;
 }
